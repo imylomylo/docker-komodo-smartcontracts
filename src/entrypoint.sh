d685d2f73cb387b9b06ac3fa464eb782e52a0501
Mylo <imylomylo@gmail.com>
2018-08-29 08:01:26 +0200

    Untested for now, added user sudo file, and passwd command in entrypoint to allow passwordless user to sudo to root to run ngrep etc.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -8,6 +8,7 @@ USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+passwd -d user
 export HOME=/home/user
 echo "HOME is ${HOME}"
 exec /usr/local/bin/gosu user "/bin/bash"
